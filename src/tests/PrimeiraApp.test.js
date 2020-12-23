112333ff9e0c35e2c17d7599a3a8349b48239eec
Andre Marcelino <marcelinoandre@gmail.com>
2020-12-22 22:58:54 -0300

    chore: add enzyme

--- src/tests/PrimeiraApp.test.js
+++ src/tests/PrimeiraApp.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
 
 describe('PrimeiraApp', () => {
-  it('should show the message  "Hello World!" ', () => {
-    const message = 'Hello World!';
-    const { getByText } = render(<PrimeraApp saludo={message} />);
-    expect(getByText(message)).toBeInTheDocument();
+  // it('should show the message  "Hello World!" ', () => {
+  //   const message = 'Hello World!';
+  //   const { getByText } = render(<PrimeraApp saludo={message} />);
+  //   expect(getByText(message)).toBeInTheDocument();
+  // });
+
+  it('should show the message "ola mundo', () => {
+    const wrapper = shallow(<PrimeraApp saludo={'ola mundo'} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
